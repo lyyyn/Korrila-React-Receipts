ce9b47f0fe8582dd1ef6f447963d2dcfddaade15
Linawaty <linawatylim@gmail.com>
2020-07-09 19:56:38 +0800

    Complete the data render of receipt

--- app.js
+++ app.js
@@ -2,14 +2,44 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            
+            receipts
         }
     }
 
     render() {
         return (
             <div>
-                Korilla
+                {this.state.receipts.map(receipt => {
+                    return (
+                        <div className="receipt-slip">
+                            <h2>{receipt.person}</h2>
+                            <p>
+                                <span className="order-title">Main: </span>
+                                <span className="order-detail">{receipt.order.main}</span>
+                            </p>
+                            <p>
+                                <span className="order-title">Protein: </span>
+                                <span className="order-detail">{receipt.order.protein}</span>
+                            </p>
+                            <p>
+                                <span className="order-title">Rice: </span>
+                                <span className="order-detail">{receipt.order.rice}</span>
+                            </p>
+                            <p>
+                                <span className="order-title">Sauce: </span>
+                                <span className="order-detail">{receipt.order.sauce}</span>
+                            </p>
+                            <p>
+                                <span className="order-title">Drink: </span>
+                                <span className="order-detail">{receipt.order.drink}</span>
+                            </p>
+                            <p>
+                                <span className="order-title">Cost: </span>
+                                <span className="order-detail">{receipt.order.cost}</span>
+                            </p>
+                        </div>
+                    )
+                })}
             </div>
         )
     }
