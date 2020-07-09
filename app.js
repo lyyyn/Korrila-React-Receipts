22ab466e57306db6f8374e036b899a2cda259c33
Linawaty <linawatylim@gmail.com>
2020-07-09 21:17:07 +0800

    Using conditoning to only show the unpaid receipts

--- app.js
+++ app.js
@@ -10,36 +10,38 @@ class App extends React.Component {
         return (
             <div>
                 <div className="all-receipts">
-                    {this.state.receipts.map(receipt => {
-                        return (
-                            <div className="receipt-slip">
-                                <h2>{receipt.person}</h2>
-                                <p>
-                                    <span className="order-title">Main: </span>
-                                    <span className="order-detail">{receipt.order.main}</span>
-                                </p>
-                                <p>
-                                    <span className="order-title">Protein: </span>
-                                    <span className="order-detail">{receipt.order.protein}</span>
-                                </p>
-                                <p>
-                                    <span className="order-title">Rice: </span>
-                                    <span className="order-detail">{receipt.order.rice}</span>
-                                </p>
-                                <p>
-                                    <span className="order-title">Sauce: </span>
-                                    <span className="order-detail">{receipt.order.sauce}</span>
-                                </p>
-                                <p>
-                                    <span className="order-title">Drink: </span>
-                                    <span className="order-detail">{receipt.order.drink}</span>
-                                </p>
-                                <p>
-                                    <span className="order-title">Cost: </span>
-                                    <span className="order-detail">{receipt.order.cost}</span>
-                                </p>
-                            </div>
-                        )
+                    {this.state.receipts.map((receipt,idx) => {
+                        if (!receipt.paid) {
+                            return (
+                                <div className="receipt-slip">
+                                    <h2>{receipt.person}</h2>
+                                    <p>
+                                        <span className="order-title">Main: </span>
+                                        <span className="order-detail">{receipt.order.main}</span>
+                                    </p>
+                                    <p>
+                                        <span className="order-title">Protein: </span>
+                                        <span className="order-detail">{receipt.order.protein}</span>
+                                    </p>
+                                    <p>
+                                        <span className="order-title">Rice: </span>
+                                        <span className="order-detail">{receipt.order.rice}</span>
+                                    </p>
+                                    <p>
+                                        <span className="order-title">Sauce: </span>
+                                        <span className="order-detail">{receipt.order.sauce}</span>
+                                    </p>
+                                    <p>
+                                        <span className="order-title">Drink: </span>
+                                        <span className="order-detail">{receipt.order.drink}</span>
+                                    </p>
+                                    <p>
+                                        <span className="order-title">Cost: </span>
+                                        <span className="order-detail">{receipt.order.cost}</span>
+                                    </p>
+                                </div>
+                            )
+                        }
                     })}
                 </div>
             </div>
