252e58754984301321f17ca26f6834b6334d6d3b
Linawaty <linawatylim@gmail.com>
2020-07-09 19:43:47 +0800

    Initial setup of React

---
+++ app.js
@@ -0,0 +1,21 @@
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                Korilla
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('.container')
+)
